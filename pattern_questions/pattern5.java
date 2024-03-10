d998e508d324609eaa923211d0b89cad3a9bb4d8
Chirag H M <chiraghm2004@gmail.com>
2024-03-11 01:33:34 +0530

    pattern5

---
+++ pattern_questions/pattern5.java
@@ -0,0 +1,26 @@
+package pattern_questions;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                int sum=i+j;
+                if (sum%2==0) {  
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//output
+
+//    1 
+//    0 1
+//    1 0 1
+//    0 1 0 1
+//    1 0 1 0 1

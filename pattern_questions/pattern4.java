19114c72c37104bfd1316b26bc11651f5836f1b7
Chirag H M <chiraghm2004@gmail.com>
2024-03-11 01:25:33 +0530

    pattern4

---
+++ pattern_questions/pattern4.java
@@ -0,0 +1,22 @@
+package pattern_questions;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        int n=5;
+        int number=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+
+//  1 
+//  2 3
+//  4 5 6
+//  7 8 9 10
+//  11 12 13 14 15

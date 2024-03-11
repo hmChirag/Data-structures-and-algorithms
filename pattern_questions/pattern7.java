e4287644e82d5fdee17994aad129a6a8012c4545
Chirag H M <chiraghm2004@gmail.com>
2024-03-12 00:34:48 +0530

    pattern7

---
+++ pattern_questions/pattern7.java
@@ -0,0 +1,24 @@
+package pattern_questions;
+public class pattern7 {
+
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=5;i>0;i--){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//  output
+
+//            *****
+//           *****
+//          *****
+//         *****
+//        *****

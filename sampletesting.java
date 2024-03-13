08e7ac4eadbf2f74be41c69360e79e50620524e1
Chirag H M <chiraghm2004@gmail.com>
2024-03-14 01:22:45 +0530

    practice-file

---
+++ sampletesting.java
@@ -0,0 +1,33 @@
+// This file is for testing and practicing different codes while learning..
+import java.util.HashMap;
+
+public class sampletesting {
+    public static void main(String[] args) {
+        // HashSet<Integer> demo = new HashSet<>();
+
+        // demo.add(1);
+        // demo.add(2);
+        // demo.add(3);
+        // demo.add(4);
+
+        // if (demo.contains(13)) {
+        //     System.out.println("present");
+        // }
+        // else{
+        //     System.out.println("not present");
+        // }
+
+        // System.out.println(demo.size());
+
+        // demo.remove(1);
+
+        // System.out.println(demo.size());
+        // System.out.println(demo);
+
+        HashMap<String,Integer> demo = new HashMap<>();
+        demo.put("apple", 120);
+        demo.put("pineapple", 150);
+        demo.put("kiwi", 180);
+        System.out.println(demo);
+    }
+}

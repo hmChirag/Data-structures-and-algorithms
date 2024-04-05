ac15c93f742a78ff2a853087069cee69c158b5b5
Chirag H M <chiraghm2004@gmail.com>
2024-04-05 13:17:43 +0530

    two sum

---
+++ arrays/two_sum.java
@@ -0,0 +1,88 @@
+
+/*------------------------------------------------1. Two Sum on leetcode--------------------------------------------
+
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists. 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+*/
+package arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*solution 1 is as below */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        int c = 0;
+
+        for(int i = 0; i < n; i++){
+            c = target - nums[i];
+            if(numMap.containsKey(c)){
+                return new int[]{numMap.get(c), i};
+            }
+
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+
+    }
+}
+
+
+
+/*solution 2 is as below */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int digit=target-nums[i];
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[j]==digit){
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution"); 
+    }
+}
+
+
+public class two_sum {
+    public static void main(String[] args) {   
+    }
+}

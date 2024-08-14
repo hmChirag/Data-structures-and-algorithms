e6c8e05cb0230c4e4a52f5d1cbd379e6561714bb
Chirag H M <chiraghm2004@gmail.com>
2024-08-14 13:52:21 +0530

    remove duplicates in the string

---
+++ arrays/duplicatesInString.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class duplicatesInString {
+    public static void removeDuplicates(String str,int idx,StringBuilder newstr,boolean map[]){
+            if(idx==str.length()){
+                System.out.println(newstr);
+                return;
+            }
+            char currChar = str.charAt(idx);
+            if (map[currChar-'a']==true) {
+                removeDuplicates(str, idx+1, newstr, map);
+            }
+            else{
+                map[currChar-'a']=true;
+                removeDuplicates(str, idx, newstr.append(currChar), map);
+            }
+    }
+    public static void main(String[] args) {
+        String str="appnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""),new boolean[26]);
+    }
+}
